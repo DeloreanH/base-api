2b60c83031137b3717cfa5ec6c4f2cd844244e1a
Harry Perez <harryy1242@gmail.com>
2020-01-12 17:58:36 -0400

    fix sector schema

--- src/database/schemas/sector.schema.ts
+++ src/database/schemas/sector.schema.ts
@@ -1,4 +1,5 @@
 import { Schema } from 'mongoose';
+import { modelName } from '../model-names';
 
 export const sectorSchema = new Schema({
     name: {
@@ -11,7 +12,8 @@ export const sectorSchema = new Schema({
     pendingUntil: {
         type: Number,
     },
-    weatherType: {
-        type: String,
+    weatherId: {
+        type: Schema.Types.ObjectId,
+        ref: modelName.WEATHER,
     },
 }, {timestamps: true});
